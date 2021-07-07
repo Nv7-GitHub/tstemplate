ac1fe2c99c6e37f8dd79b8c822b99ee04d357907
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-07-07 07:33:34 -0700

    Add template

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/* Bootstrap */
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+/* Bootstrap */
+
+/* Your Code Below */
+let p = document.createElement("p");
+p.innerText = "Hello, Wordl!";
+p.classList.add("lead");
+document.body.appendChild(p);
